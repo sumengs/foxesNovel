dbabed977e4580069b915cc353f6f94907d4f4e3
sumeng <15170830271@163.com>
2020-06-27 10:04:38 +0800

    1. 服务子模块搭建 2. common和common-db模块搭建

---
+++ foxes-common/src/main/java/com/sumeng/peekshopping/entity/Result.java
@@ -0,0 +1,54 @@
+package com.sumeng.peekshopping.entity;
+
+import com.sumeng.peekshopping.constant.StatusCode;
+import lombok.Data;
+
+/**
+ * 返回结果实体类
+ *
+ * @date: 2020/6/27 9:41
+ * @author: sumeng
+ */
+@Data
+public class Result<T> {
+
+    /**
+     * 是否成功
+     */
+    private boolean flag;
+
+    /**
+     * 返回码
+     */
+    private Integer code;
+
+    /**
+     * 返回消息
+     */
+    private String message;
+
+    /**
+     *
+     */
+    private T data;
+
+
+    public Result(boolean flag, Integer code, String message, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+        this.data = (T) data;
+    }
+
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result() {
+        this.flag = true;
+        this.code = StatusCode.OK;
+        this.message = "执行成功";
+    }
+}
---
+++ foxes-service/foxes-search-service/src/main/java/com/foxes/chapter/SearchServiceApplication.java
@@ -0,0 +1,8 @@
+package com.foxes.chapter;
+
+/**
+ * @date: 2020/6/27 10:01
+ * @author: sumeng
+ */
+public class SearchServiceApplication {
+}
