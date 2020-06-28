8c7518b524bdae91a95c043998e7d9160aa5cc24
kingwin <343387120@qq.com>
2020-06-28 10:00:43 +0800

    search-1.0

--- foxes-service/foxes-search-service/src/main/java/com/foxes/chapter/SearchServiceApplication.java
+++ foxes-service/foxes-search-service/src/main/java/com/foxes/chapter/SearchServiceApplication.java
@@ -1,8 +1,17 @@
 package com.foxes.chapter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @date: 2020/6/27 10:01
  * @author: sumeng
  */
+
+@SpringBootApplication
 public class SearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class,args);
+    }
 }
