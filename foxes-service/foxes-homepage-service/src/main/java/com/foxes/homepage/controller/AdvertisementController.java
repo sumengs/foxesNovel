4a711d4d74850568b7333766165b76f91d61ba86
sumeng <15170830271@163.com>
2020-07-03 18:55:04 +0800

    1. homepage redis 缓存

--- foxes-service/foxes-homepage-service/src/main/java/com/foxes/homepage/controller/AdvertisementController.java
+++ foxes-service/foxes-homepage-service/src/main/java/com/foxes/homepage/controller/AdvertisementController.java
@@ -2,6 +2,7 @@ package com.foxes.homepage.controller;
 
 import com.foxes.book.pojo.Book;
 import com.foxes.chapter.pojo.Advertisement;
+import com.foxes.chapter.pojo.Leaderboard;
 import com.foxes.homepage.service.AdvertisementService;
 import com.sumeng.peekshopping.constant.StatusCode;
 import com.sumeng.peekshopping.entity.Result;
@@ -37,7 +38,7 @@ public class AdvertisementController {
     public Result<List<Advertisement>> smallImage() {
 
         List<Advertisement> smallImage = advertisementService.smallImage();
-        return new Result<>(true, StatusCode.OK, "轮播图数据返回成功", smallImage);
+        return new Result<>(true, StatusCode.OK, "小横幅返回成功", smallImage);
     }
 
 
@@ -45,26 +46,30 @@ public class AdvertisementController {
     @ResponseBody
     public Result<Advertisement> bigImage() {
         Advertisement bigImage = advertisementService.bigImage();
-        return new Result<>(true, StatusCode.OK, "轮播图数据返回成功", bigImage);
+        return new Result<>(true, StatusCode.OK, "大横幅返回成功", bigImage);
     }
+
     @RequestMapping("/weixin")
     @ResponseBody
-    public Result<Advertisement> weixinImage(){
+    public Result<Advertisement> weixinImage() {
         Advertisement weixinImage = advertisementService.weixinImage();
-        return new Result<>(true,StatusCode.OK,"微信二维码返回成功",weixinImage);
+        return new Result<>(true, StatusCode.OK, "微信二维码返回成功", weixinImage);
     }
 
 
     @ResponseBody
     @RequestMapping("/subscribe")
     public Result<List<Book>> subscribeList() {
-        return advertisementService.subscribeList();
+        List<Leaderboard> books = advertisementService.subscribeList();
+        return new Result<>(true, StatusCode.OK, "订阅榜前10返回成功", books);
     }
 
 
     @ResponseBody
     @RequestMapping("/read")
     public Result<List<Book>> readList() {
-        return advertisementService.readList();
+        List<Leaderboard> books = advertisementService.readList();
+        return new Result<>(true, StatusCode.OK, "热度榜前10返回成功", books);
     }
+
 }
